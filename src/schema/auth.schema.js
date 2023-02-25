86f5331ffb56c69c7c154e85ebb9bf907680f397
kendy-karakawa <kendy.karakawa@gmail.com>
2023-02-25 14:00:48 -0300

    fix: auth.schema

--- src/schema/auth.schema.js
+++ src/schema/auth.schema.js
@@ -1,13 +1,13 @@
 import joi from "joi";
 
 export const signUpSchema = joi.object({
-    name: joi.string().alphanum().min(3).max(10).required(),
+    name: joi.string().required(),
     email: joi.string().email({ minDomainSegments: 2, tlds: { allow: ["com", "net", "br"] } }).required(),
-    password: joi.string().alphanum().min(3).max(10).required(),
+    password: joi.string().required(),
     confirmPassword: joi.any().valid(joi.ref('password')).required()
   });
 
   export const signInSchema = joi.object({
     email: joi.string().email({ minDomainSegments: 2, tlds: { allow: ["com", "net", "br"] } }).required(),
-    password: joi.string().alphanum().min(3).max(10).required()
+    password: joi.string().required()
   });
