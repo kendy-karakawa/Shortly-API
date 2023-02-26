4bbe72c479127e4fce20e7ce75d1ba413ae5ec99
kendy-karakawa <kendy.karakawa@gmail.com>
2023-02-25 21:03:00 -0300

    fix: ranking

--- src/controller/rank.controller.js
+++ src/controller/rank.controller.js
@@ -4,7 +4,7 @@ export async function ranking(req, res){
 
     try {
         const top10 = await db.query(`
-        SELECT u.id, u.name, COUNT(s.url) as "linksCount", SUM(s.visit_count) as visitCount
+        SELECT u.id, u.name, COUNT(s.url) as "linksCount", SUM(s.visit_count) as "visitCount"
         FROM users u 
         JOIN shorten s ON u.id = s.user_id
         GROUP BY u.id
